646bb727a3087406b5db7be25f6625b1f4c72fbe
TechDev <89547375+TechDevOfficial@users.noreply.github.com>
2026-06-24 18:57:51 +0200

    Bump version to 1.0.1

--- Manactor/ManactorPlugin.cs
+++ Manactor/ManactorPlugin.cs
@@ -9,7 +9,7 @@ namespace ClassicUs.Manactor
     public class ManactorPlugin : BasePlugin
     {
         public const string Guid = "classicus.manactor";
-        public const string Version = "1.0.0";
+        public const string Version = "1.0.1";
 
         public static ManualLogSource Log;
 
